d49bc32c5a6e67989bfde37529711f847dc00348
Арсений Казанцев <kazars24@MacBook-Air-Arsenij.local>
2023-09-07 16:52:33 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+
+possible_cities = ["", "Москва", "Санкт-Петербург", "Нижний Новгород"]
+
 st.set_page_config(
     page_title="Customer Feedback Platform",
     page_icon="👥",
@@ -24,30 +27,35 @@ with st.expander("ℹ️ - About this app", expanded=True):
 st.markdown("")
 st.markdown("## 📌 Ниже рабочая область")
 
-with st.form(key="my_form"):
-    c1, c2 = st.columns([2, 5])
-    with c1:
-        ModelType = st.radio(
-            "Выберете масштаб",
+c1, c2 = st.columns([2, 5])
+with c1:
+
+    with st.form(key="scale_form"):
+        scale = st.radio(
+            "Выберите масштаб",
             ["Магазин", "Район", "Город", "Регион"],
             help="Тут нужно выбрать один из предложенных вариантов, в рамках которого требуется провести анализ отзывов.",
         )
-        submit_button1 = st.form_submit_button(label="Подтвердить выбор")
-        if submit_button1:
-            if ModelType == "Магазин":
-                shop_address = st.text_input("Введите адрес магазина:", help="Введите адрес магазина вручную.")
+        scale_submit_button1 = st.form_submit_button(label="Подтвердить выбор")
 
-            elif ModelType == "Район":
-                district = st.text_input("Введите город и название нужного района:",
-                                         help="Введите город и район вручную.")
-
-            elif ModelType == "Город":
-                city = st.text_input("Введите город:", help="Введите город вручную.")
-
-            else:
-                region = st.text_input("Введите регион:", help="Введите регион вручную.")
-
-    submit_button2 = st.form_submit_button(label="Подтвердить выбор")
+    with st.form(key="name_form"):
+        if scale == "Магазин":
+            name = st.text_input(
+                "Введите адрес магазина:", 
+                help="Введите адрес магазина вручную.")
+        elif scale == "Район":
+            name = st.text_input(
+                "Введите район:", 
+                help="Введите район вручную.")
+        elif scale == "Город":
+            city_name = st.selectbox('Выберите город: ',possible_cities)
+        else:
+            manual_address = st.text_input(
+                "Введите регион:", 
+                help="Введите регион вручную.")
+        scale_submit_button2 = st.form_submit_button(label="Подтвердить выбор")
+            
+            
 
     with c2:
         st.title('Тут будут реультаты')
