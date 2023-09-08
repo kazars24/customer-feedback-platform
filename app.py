25a0d343e63f6f0c650bfdf72ac79078643e32d1
Арсений Казанцев <kazars24@MacBook-Air-Arsenij.local>
2023-09-08 22:09:59 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+from statistics import mean
 import streamlit as st
 import pydeck as pdk
 from geopy.geocoders import Nominatim
@@ -8,6 +10,22 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.llm.agent import Agent
+
+
+def data_preprocessing(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    map_data = {}
+    for v in data.values():
+        map_data[v.get("address")] = [review['rating'] for review in v['reviews_list']]
+    for key, values in map_data.items():
+        if values:
+            average = sum(values) / len(values)
+            map_data[key] = average
+        else:
+            map_data[key] = 0
+    return map_data
+
 
 possible_cities = ["", "Москва", "Санкт-Петербург", "Нижний Новгород"]
 
@@ -61,55 +79,64 @@ with c1:
     with c2:
         st.title('Тут будут результаты')
 
-        model_path = "D:\\llama.cpp\\models\\7b\\ggml-model-q4_1.bin"
-        overall_sys_prompt = 'Ты — русскоязычный автоматический анализатор отзывов покупателей. Ты получаешь на вход текст нескольких отзывов одного филиала. Сделай короткий вывод о филиале.'
-        review_overall_agent = Agent(model_path, overall_sys_prompt)
+        if st.button('Анализировать'):
 
-        request = '''
-                Отзыв 1: Свежие фрукты и овощи, чистый и приятный магазин, располагается рядом с домом, хороший выбор товаров, хорошие цены, быстрое обслуживание, нет очередей, вежливый персонал. Отзыв 2: Нет очередей, хороший выбор товаров, хорошие цены, располагается рядом с домом, вежливый персонал, быстрое обслуживание, свежие фрукты и овощи, чистый и приятный магазин. Вежливый персонал. Быстро нашла что мне нужно. Отличные продукты, свежие. Отзыв 3: Невысокие цены. Персонал приветливый. Отзыв 4: Можно купить кофе, капучино, флэт уайт. Даже ночью)
-            '''
-        st.text(review_overall_agent.interact(request))
+            model_path = "D:\\llama.cpp\\models\\7b\\ggml-model-q4_1.bin"
+            overall_sys_prompt = 'Ты — русскоязычный автоматический анализатор отзывов покупателей. Ты получаешь на вход текст нескольких отзывов одного филиала. Сделай короткий вывод о филиале.'
+            review_overall_agent = Agent(model_path, overall_sys_prompt)
 
-        # список адресов
-        addresses = ['Ваш адрес 1', 'Ваш адрес 2', 'Ваш адрес 3']
-        ratings = np.array([1.2, 3.1, 4.8])
+            request = '''
+                    Отзыв 1: Свежие фрукты и овощи, чистый и приятный магазин, располагается рядом с домом, хороший выбор товаров, хорошие цены, быстрое обслуживание, нет очередей, вежливый персонал. Отзыв 2: Нет очередей, хороший выбор товаров, хорошие цены, располагается рядом с домом, вежливый персонал, быстрое обслуживание, свежие фрукты и овощи, чистый и приятный магазин. Вежливый персонал. Быстро нашла что мне нужно. Отличные продукты, свежие. Отзыв 3: Невысокие цены. Персонал приветливый. Отзыв 4: Можно купить кофе, капучино, флэт уайт. Даже ночью)
+                '''
+            st.text(review_overall_agent.interact(request))
 
-        geolocator = Nominatim(user_agent="myGeocoder")
+            #map_data = data_preprocessing('JSON FILES')
+            #addresses = np.array(map_data.keys())
+            #ratings = np.array(map_data.values())
+            addresses = ["Большая Московская, 71, Октябрьский район, Владимир, 600000",
+                         "Проспект Строителей, 9 к4, Ленинский район, Владимир, 600014",
+                         "Северная, 3а, Фрунзенский район, Владимир, 600007"]
+            ratings = np.array([1.2, 3.1, 4.8])
 
-        lats = []
-        longs = []
-        for address in addresses:
-            location = geolocator.geocode(address)
-            lats.append(location.latitude)
-            longs.append(location.longitude)
+            geolocator = Nominatim(user_agent="myGeocoder")
 
-        # создаем фрейм данных с адресами и рейтингами
-        df = pd.DataFrame({
-            'Адрес': addresses,
-            'Широта': lats,
-            'Долгота': longs,
-            'Рейтинг': ratings
-        })
+            lats = []
+            longs = []
+            for address in addresses:
+                location = geolocator.geocode(address)
+                print(location)
+                lats.append(location.latitude)
+                longs.append(location.longitude)
 
-        # определяем цвета для рейтингов
-        color_scale = np.array(['red', 'orange', 'yellow', 'green', 'blue'])
-        df['color'] = pd.cut(df['Рейтинг'], bins=[0., 1., 2., 3., 4., 5.], labels=color_scale)
+            # создаем фрейм данных с адресами и рейтингами
+            df = pd.DataFrame({
+                'Адрес': addresses,
+                'Широта': lats,
+                'Долгота': longs,
+                'Рейтинг': ratings
+            })
 
-        st.pydeck_chart(pdk.Deck(
-            map_style='mapbox://styles/mapbox/light-v9',
-            initial_view_state=pdk.ViewState(
-                latitude=np.mean(df['Широта']),
-                longitude=np.mean(df['Долгота']),
-                zoom=11,
-                pitch=50,
-            ),
-            layers=[
-                pdk.Layer(
-                    'ScatterplotLayer',
-                    data=df,
-                    get_position='[Долгота, Широта]',
-                    get_color='color',
-                    get_radius=200,
+            # определяем цвета для рейтингов
+            color_scale = np.array(['red', 'orange', 'yellow', 'green', 'blue'])
+            df['color'] = pd.cut(df['Рейтинг'], bins=[0., 1., 2., 3., 4., 5.], labels=color_scale)
+
+            st.pydeck_chart(pdk.Deck(
+                map_style='mapbox://styles/mapbox/light-v9',
+                initial_view_state=pdk.ViewState(
+                    latitude=np.mean(df['Широта']),
+                    longitude=np.mean(df['Долгота']),
+                    zoom=11,
+                    pitch=50,
                 ),
-            ],
-        ))
+                layers=[
+                    pdk.Layer(
+                        'ScatterplotLayer',
+                        data=df,
+                        get_position='[Долгота, Широта]',
+                        get_color='color',
+                        get_radius=200,
+                    ),
+                ],
+            ))
+
+
